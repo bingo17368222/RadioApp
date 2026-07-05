4372f62af1fe6b7f68f5698d1114bdb544ad2318
Radio Dev <dev@radio.app>
2026-07-05 07:54:59 +0000

    fix: if expression needs block when used as statement

--- app/src/main/java/com/radio/app/activities/PlayerActivity.kt
+++ app/src/main/java/com/radio/app/activities/PlayerActivity.kt
@@ -616,7 +616,7 @@ class PlayerActivity : AppCompatActivity() {
                             val startPos = if (pendingSeekMs > 0) pendingSeekMs else -1L
                             writeJitterLog("onServiceConnected: isFreshStart, playEpisode with startPos=$startPos (pendingSeekMs=$pendingSeekMs)")
                             playbackService?.playEpisode(episode, false, startPos)
-                            if (pendingSeekMs > 0) pendingSeekMs = -1L  // Clear after use
+                            if (pendingSeekMs > 0) { pendingSeekMs = -1L }  // Clear after use
                         } else {
                             var savedPosition = playbackService?.getSavedPositionForEpisode(episode) ?: -1L
                             // [v2.0.76] Issue 1 Fix: When restoring from killed service, prefer Activity's cached
